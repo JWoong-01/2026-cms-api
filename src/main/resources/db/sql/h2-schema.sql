6c65117ef3dbdfb6f3c00d9c730da940b5a05f60
JWoong-01 <yju407@naver.com>
2026-03-19 01:53:33 +0900

    feat: DB 초기화 및 예외 처리 추가

--- src/main/resources/db/sql/h2-schema.sql
+++ src/main/resources/db/sql/h2-schema.sql
@@ -1,9 +1,22 @@
--- TODO schema
--- example
-create table members
+create table users
 (
     id                 bigint primary key      not null auto_increment,
-    name               varchar(50)             not null,
+    username           varchar(50)             not null unique,
+    password           varchar(255)            not null,
+    role               varchar(20)             not null,
+    enabled            boolean                 not null,
     created_date       timestamp default now() not null,
     last_modified_date timestamp
+);
+
+create table contents
+(
+    id                 bigint primary key      not null auto_increment,
+    title              varchar(100)            not null,
+    description        text,
+    view_count         bigint                  not null,
+    created_date       timestamp default now() not null,
+    created_by         varchar(50)             not null,
+    last_modified_date timestamp,
+    last_modified_by   varchar(50)
 );
